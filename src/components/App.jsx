d35581c3f27b5dfb85bc4e807ffd917709399756
Romalyy <1303roma01@gmail.com>
2022-07-07 18:51:59 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,107 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from "react";
+
+import s from "./ContactList/ContactList.module.css";
+
+import { v4 as uuidv4 } from "uuid";
+import ContactList from "./ContactList/ContactList";
+import Filter from "./Filter/Filter";
+import ContactForm from "./ContactForm/ContactForm";
+
+export default class App extends Component {
+  state = {
+    contacts: [
+      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+    ],
+    filter: "",
+  };
+
+  addContact = (el) => {
+    const searchName = this.state.contacts
+      .map((contact) => contact.name)
+      .includes(el.name);
+
+    if (searchName) {
+      alert(`${el.name} is already in contacts`);
+    } else if (el.name.length === 0) {
+      alert("Fields must be filled!");
+    } else {
+      const contact = {
+        ...el,
+        id: uuidv4(),
+      };
+
+      this.setState((prevState) => ({
+        contacts: [...prevState.contacts, contact],
+      }));
+    }
+  };
+
+  changeFilter = (filter) => {
+    this.setState({ filter });
+  };
+
+  getVisibleContacts = () => {
+    const { contacts, filter } = this.state;
+
+    if (!filter) {
+      return contacts;
+    }
+
+    return contacts.filter((contacts) =>
+      contacts.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  };
+
+  removeContact = (contactId) => {
+    this.setState((prevState) => {
+      return {
+        contacts: prevState.contacts.filter(({ id }) => id !== contactId),
+      };
+    });
+  };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+
+    
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+    }
+  }
+
+  render() {
+    const { filter, contacts } = this.state;
+
+    const visibleContacts = this.getVisibleContacts();
+
+    return (
+      <div className={s.container}>
+        <h1 className={s.title}>Phonebook</h1>
+
+        <ContactForm onSubmit={this.addContact} />
+        <h2 className={s.title}>Contacts</h2>
+        {contacts.length > 1 && (
+          <Filter value={filter} onChangeFilter={this.changeFilter} />
+        )}
+        {contacts.length > 0 && (
+          <ContactList
+            contacts={visibleContacts}
+            onRemoveContact={this.removeContact}
+          />
+        )}
+      </div>
+    );
+  }
+}
